e9140b007e743178037bf01558d24b0d27b5f3f9
Sangeetha SR <sangeetharajan567@gmail.com>
2024-08-27 00:19:45 +0530

    Changed sidebar menu

--- src/sideBar/Sidebar.jsx
+++ src/sideBar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../images/dashboardLogo.png";
+import manageIcon from "../images/manageIcon.svg";
+import dotsIcon from "../images/dotsIcon.png";
 import Design from "./Sidebar.module.css";
 
 export default function Sidebar() {
@@ -11,55 +13,55 @@ export default function Sidebar() {
       <ul className={Design.sidebarList}>
         <li className={Design.listHeading}>Create</li>
         <li>
-          <div className={Design.sidebarContent}>CRM Management</div>
+          <div className={Design.sidebarContent}><img src={manageIcon}></img> CRM Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Sales Order Management</div>
+          <div className={Design.sidebarContent}><span/>Sales Order Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Finanace Management</div>
+          <div className={Design.sidebarContent}><span/>Finanace Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>HR Management</div>
+          <div className={Design.sidebarContent}><span/>HR Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Procurement</div>
+          <div className={Design.sidebarContent}><span/>Procurement</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Asset Management</div>
+          <div className={Design.sidebarContent}><span/>Asset Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Inventory Management</div>
+          <div className={Design.sidebarContent}><span/>Inventory Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Project Management</div>
+          <div className={Design.sidebarContent}><span/>Project Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Helpdesk Management</div>
+          <div className={Design.sidebarContent}><span/>Helpdesk Management</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Business Intelligencee</div>
+          <div className={Design.sidebarContent}><span/>Business Intelligencee</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Control Panel</div>
+          <div className={Design.sidebarContent}><span/>Control Panel</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Application Settings</div>
+          <div className={Design.sidebarContent}><span/>Application Settings</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Solution Builder</div>
+          <div className={Design.sidebarContent}><span/>Solution Builder</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Product & Services</div>
+          <div className={Design.sidebarContent}><span/>Product & Services</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Suppliers & Customers</div>
+          <div className={Design.sidebarContent}><span/>Suppliers & Customers</div>
         </li>
         <li>
-          <div className={Design.sidebarContent}>Organization</div>
+          <div className={Design.sidebarContent}><span/>Organization</div>
         </li>
         <li>
-          <div className={Design.sidebarMoreBtn}>More items</div>
+          <div className={Design.sidebarMoreBtn}><img src={dotsIcon}></img>More items</div>
         </li>
         <li>
           <div className={Design.sidebarContentLogo}>
