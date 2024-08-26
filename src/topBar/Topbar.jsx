c13656a5096f96e5d58663d8b42ea260c2d451ea
Sangeetha SR <sangeetharajan567@gmail.com>
2024-08-27 03:40:38 +0530

    Completed responsive topbar

--- src/topBar/Topbar.jsx
+++ src/topBar/Topbar.jsx
@@ -4,7 +4,9 @@ import {
   BellOutlined,
   MailOutlined,
   QuestionCircleOutlined,
+  MenuOutlined,
 } from "@ant-design/icons";
+import Logo from "../images/dashboardLogo.png";
 import ProfileBadge from "../components/ProfileBadge";
 import SearchInput from "../components/SearchInput";
 import Design from "./Topbar.module.css";
@@ -12,35 +14,40 @@ import Design from "./Topbar.module.css";
 export default function Topbar() {
   return (
     <div className={Design.topbar}>
+
+      <div className={Design.mobileTopbar}>
+        <div className={Design.openSidebarBtn}>
+          <MenuOutlined />
+        </div>
+        <img src={Logo} className={Design.topbarLogo} />
+      </div>
+
+
       <div className={Design.topbarActions}>
-       
-       <SearchInput />
+        <SearchInput />
 
         <ul className={Design.topbarAlerts}>
           <li>
-            <QuestionCircleOutlined />
-            {" "}
-            Help
+            <QuestionCircleOutlined /> Help
           </li>
           <li>
             <Badge count={5}>
               <Avatar icon={<BellOutlined />} shape="circle" size={30} />
-            </Badge>
-            {" "}
+            </Badge>{" "}
             Alerts
           </li>
           <li>
             <Badge count={5}>
               <Avatar icon={<MailOutlined />} shape="circle" size={30} />
-            </Badge>
-            {" "}
+            </Badge>{" "}
             Email
           </li>
         </ul>
       </div>
 
-          <ProfileBadge />
-      
+      <div className={Design.ProfileBadgeContainer}>
+        <ProfileBadge />
+      </div>
     </div>
   );
 }
