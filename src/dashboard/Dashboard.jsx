453c19cc93325e9bb3a470c6386d52445f5e0c47
Sangeetha SR <sangeetharajan567@gmail.com>
2024-08-27 03:14:20 +0530

    Fixed responsiveness of dashboard

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import DashboardOverviewCards from "./DashboardOverviewCards";
 import RequestForPurchaseCard from "../requestForPurchaseCard/RequestForPurchaseCard";
-import EmptyDashboardCards from "../dashboard/EmptyDashboardCards"
+import EmptyDashboardCards from "../dashboard/EmptyDashboardCards";
 import Design from "./Dashboard.module.css";
 
 export default function Dashboard() {
   return (
     <div className={Design.dashboard}>
-      
       <DashboardOverviewCards />
 
       <div className={Design.dashboardDataContainer}>
+
         <div className={Design.dashboardData}>
-          <RequestForPurchaseCard />
-          <EmptyDashboardCards title={"Upcoming Tasks"}/>
+          <div className={Design.dashboardDataCard}>
+            <RequestForPurchaseCard />
+          </div>
+          <div className={Design.dashboardDataCard}>
+            <EmptyDashboardCards title={"Upcoming Tasks"} />
+          </div>
         </div>
+
         <div className={Design.dashboardData}>
-          <EmptyDashboardCards title={"Upcoming Events"}/>
-          <EmptyDashboardCards title={"Net Income()"}/>
+          <div className={Design.dashboardDataCard}>
+            <EmptyDashboardCards title={"Upcoming Events"} />
+          </div>
+          <div className={Design.dashboardDataCard}>
+            <EmptyDashboardCards title={"Net Income()"} />
+          </div>
         </div>
+        
       </div>
     </div>
   );
